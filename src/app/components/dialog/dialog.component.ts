1e16f31a61b291de83ff705fb4d3ab2b708c0c38
Francielle Abreu da Silva <fran.abreu2@gmail.com>
2024-06-13 11:25:19 +0200

    ajdust code transfer

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -26,6 +26,7 @@ export class DialogComponent {
   copyPix() {
     navigator.clipboard.writeText(
       `00020126590014BR.GOV.BCB.PIX0128danielmariadasilva@gmail.com0205${this.item.title}5204000053039865405${this.item.price}.005802BR5921Daniel Maria da Silva6009SAO PAULO62140510gFcbW8wILf6304E84D`
+      // '00020126330014BR.GOV.BCB.PIX011100769017002${this.item.title}5204000053039865802${this.item.price}.BR5921Debora Maria da Silva6009SAO PAULO62140510LL1vbbDuSN63041934'
     );
     this.firstScreen = false;
   }
